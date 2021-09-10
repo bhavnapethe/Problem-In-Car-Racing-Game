818b1b840eb0650c5ed689ae846edf15b6b67bb2
bhavnapethe <77008975+bhavnapethe@users.noreply.github.com>
2021-09-10 20:55:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+var track, car1_img, car2_img, car3_img, car4_img;
+
+function preload(){
+  track = loadImage("../images/track.jpg");
+  car1_img = loadImage("../images/car1.png");
+  car2_img = loadImage("../images/car2.png");
+  car3_img = loadImage("../images/car3.png");
+  car4_img = loadImage("../images/car4.png");
+  ground = loadImage("../images/ground.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+    gameOver();
+  }
+}
+
+function gameOver(){
+  swal({
+      title: "GAMEOVER", 
+      text: "Your Rank Is " + player.rank, 
+      imageUrl: "images/car1.png",
+      imageSize: "150x150",
+      confirmButtonText: "Play Again"
+  },
+  function(isConfirm){
+    if(isConfirm){
+      location.reload()
+      //if you want the button to press you have to write inside the bracket
+    }
+  })
+
+}
